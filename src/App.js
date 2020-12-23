8abd394a09a3122d688a130b4ca192ecde554dcb
Lokendra Dangi <pachar.com@gmail.com>
2020-12-23 11:08:26 +0545

    router is setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    //BEM Conventions
-    <div className="app">
-      <h2>Amazon clone</h2>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route to="/" exact>
+            <h2>Home Page</h2>
+          </Route>
+          <Route to="/checkout" exact>
+            <h2>checkout</h2>
+          </Route>
+          <Route to="/login" exact>
+            <h2>login</h2>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
